2c8b29c56d19d3b959000f20c1a8c200fa914987
Thomas McDonald <4001137@keranna.qc.ca>
2024-12-24 01:47:00 -0500

    Added scheduler

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include "AudioOutputI2S.h"
 #include <I2S.h>
 #include <LittleFS.h>
+#include <Scheduler.h>
+#include <Task.h>
+
 
 AudioGeneratorWAV *wav;
 AudioFileSourceLittleFS *file_armed;
@@ -22,117 +25,158 @@ void littleFsListDir(const char *dirname);
 void loop_warning_lights();
 void stop_all_lights();
 
-bool triggered = false;
-bool previousState = false;
-bool playingArmed = false;
-bool playingPulse = false;
+bool warning_lights_on = false;
+
+class AudioTask : public Task
+{
+private:
+  bool triggered = false;
+  bool previousState = false;
+  bool playingArmed = false;
+  bool playingPulse = false;
+
+protected:
+  void setup()
+  {
+    if (!I2S.begin(I2S_PHILIPS_MODE, 44100, 16))
+    {
+      while (1)
+        ; // do nothing
+    }
+    Serial.println("\nI2S Initialized");
+    if (!LittleFS.begin())
+    {
+      Serial.println("Failed to initialize LittleFS");
+    }
+    Serial.println("LittleFS Initialized");
+    littleFsListDir("/");
+    file_armed = new AudioFileSourceLittleFS("/armed.wav");
+    file_pulsearmed = new AudioFileSourceLittleFS("/pulse_armed.wav");
+    out = new AudioOutputI2S();
+    wav = new AudioGeneratorWAV();
+    out->SetBitsPerSample(16);
+    out->SetRate(44100);
+  }
+  void loop()
+  {
+    bool currentState = digitalRead(TRIGGER);
+    Serial.println(currentState);
+
+    // Only play sounds when the state changes and no sound is currently playing
+    if (!wav->isRunning())
+    {
+      // State changed from LOW to HIGH
+      if (currentState == HIGH && previousState == LOW)
+      {
+        warning_lights_on = true;
+        if (file_activate) file_activate->close();  // Close if exists
+        file_activate = new AudioFileSourceLittleFS("/activate.wav");
+        if (file_activate->isOpen()) {
+          wav->begin(file_activate, out);
+          triggered = true;
+        } else {
+          Serial.println("Failed to open activate.wav");
+        }
+      }
+      // State changed from HIGH to LOW
+      else if ((currentState == LOW && previousState == HIGH ) || (currentState == LOW && previousState == HIGH && triggered))
+      {
+        if (file_deactivate) file_deactivate->close();  // Close if exists
+        file_deactivate = new AudioFileSourceLittleFS("/deactivate.wav");
+        if (file_deactivate->isOpen()) {
+          wav->begin(file_deactivate, out);
+          triggered = false;
+        } else {
+          Serial.println("Failed to open deactivate.wav");
+        }
+      }
+      // If triggered and no sound playing, play armed.wav in loop
+      else if (triggered) {
+        if (file_armed) file_armed->close();
+        file_armed = new AudioFileSourceLittleFS("/armed.wav");
+        if (file_armed->isOpen()) {
+          wav->begin(file_armed, out);
+          triggered = true;
+        }
+      }
+    } 
+    else if (currentState == LOW && previousState == HIGH && triggered)
+    {
+      warning_lights_on = false;
+      if (file_deactivate) file_deactivate->close();  // Close if exists
+      file_deactivate = new AudioFileSourceLittleFS("/deactivate.wav");
+      if (file_deactivate->isOpen()) {
+        wav->begin(file_deactivate, out);
+        triggered = false;
+      } else {
+        Serial.println("Failed to open deactivate.wav");
+      }
+    }
+    
+    previousState = currentState;
+    if (wav->isRunning()) {
+      if (!wav->loop()) {
+        wav->stop();
+      }
+    }
+  }
+}audio_task;
+
+class LightsTask : public Task
+{
+  protected:
+    void setup()
+    {
+      pinMode(ARMED_LED, OUTPUT);
+      pinMode(WARNING_LEFT_LED, OUTPUT);
+      pinMode(WARNING_MIDDLE_LED, OUTPUT);
+      pinMode(WARNING_RIGHT_LED, OUTPUT);
+      pinMode(TRIGGER, INPUT);
+
+      digitalWrite(WARNING_LEFT_LED, LOW);
+      digitalWrite(WARNING_MIDDLE_LED, LOW);
+      digitalWrite(WARNING_RIGHT_LED, LOW);
+      digitalWrite(ARMED_LED, LOW);
+    }
+    void loop()
+    {
+      if (warning_lights_on)
+      {
+        digitalWrite(ARMED_LED, HIGH);
+        digitalWrite(WARNING_LEFT_LED, HIGH);
+        delay(300);
+        digitalWrite(WARNING_LEFT_LED, LOW);
+        digitalWrite(WARNING_MIDDLE_LED, HIGH);
+        delay(300);
+        digitalWrite(WARNING_MIDDLE_LED, LOW);
+        digitalWrite(WARNING_RIGHT_LED, HIGH);
+        delay(300);
+        digitalWrite(WARNING_RIGHT_LED, LOW);
+      } else
+      {
+        digitalWrite(WARNING_LEFT_LED, LOW);
+        digitalWrite(WARNING_MIDDLE_LED, LOW);
+        digitalWrite(WARNING_RIGHT_LED, LOW);
+        digitalWrite(ARMED_LED, LOW);
+      }      
+    }
+}lights_task;
+
 
 void setup()
 {
-  pinMode(ARMED_LED, OUTPUT);
-  pinMode(WARNING_LEFT_LED, OUTPUT);
-  pinMode(WARNING_MIDDLE_LED, OUTPUT);
-  pinMode(WARNING_RIGHT_LED, OUTPUT);
-  pinMode(TRIGGER, INPUT);
-
-  digitalWrite(WARNING_LEFT_LED, LOW);
-  digitalWrite(WARNING_MIDDLE_LED, LOW);
-  digitalWrite(WARNING_RIGHT_LED, LOW);
-  digitalWrite(ARMED_LED, LOW);
-
   Serial.begin(9600);
+  Serial.println("");
   delay(500);
-  if (!I2S.begin(I2S_PHILIPS_MODE, 44100, 16))
-  {
-    while (1)
-      ; // do nothing
-  }
-  Serial.println("\nI2S Initialized");
-  delay(500);
-  if (!LittleFS.begin())
-  {
-    Serial.println("Failed to initialize LittleFS");
-  }
-  Serial.println("LittleFS Initialized");
-  littleFsListDir("/");
-  file_armed = new AudioFileSourceLittleFS("/armed.wav");
-  file_pulsearmed = new AudioFileSourceLittleFS("/pulse_armed.wav");
-  out = new AudioOutputI2S();
-  wav = new AudioGeneratorWAV();
-  out->SetBitsPerSample(16);
-  out->SetRate(44100);
+
+  Scheduler.start(&audio_task);
+  Scheduler.start(&lights_task);
+
+  Scheduler.begin();
+
 }
 
-void loop()
-{
-  static bool warning_lights = false;
-  bool currentState = digitalRead(TRIGGER);
-  Serial.println(currentState);
-
-  // Only play sounds when the state changes and no sound is currently playing
-  if (!wav->isRunning())
-  {
-    // State changed from LOW to HIGH
-    if (currentState == HIGH && previousState == LOW)
-    {
-      digitalWrite(ARMED_LED, HIGH);
-      warning_lights = true;
-      if (file_activate) file_activate->close();  // Close if exists
-      file_activate = new AudioFileSourceLittleFS("/activate.wav");
-      if (file_activate->isOpen()) {
-        wav->begin(file_activate, out);
-        triggered = true;
-      } else {
-        Serial.println("Failed to open activate.wav");
-      }
-    }
-    // State changed from HIGH to LOW
-    else if ((currentState == LOW && previousState == HIGH ) || (currentState == LOW && previousState == HIGH && triggered))
-    {
-      if (file_deactivate) file_deactivate->close();  // Close if exists
-      file_deactivate = new AudioFileSourceLittleFS("/deactivate.wav");
-      if (file_deactivate->isOpen()) {
-        wav->begin(file_deactivate, out);
-        triggered = false;
-      } else {
-        Serial.println("Failed to open deactivate.wav");
-      }
-    }
-    // If triggered and no sound playing, play armed.wav in loop
-    else if (triggered) {
-      if (file_armed) file_armed->close();
-      file_armed = new AudioFileSourceLittleFS("/armed.wav");
-      if (file_armed->isOpen()) {
-        wav->begin(file_armed, out);
-        triggered = true;
-      }
-    }
-  } else if (currentState == LOW && previousState == HIGH && triggered)
-  {
-    stop_all_lights();
-      if (file_deactivate) file_deactivate->close();  // Close if exists
-      file_deactivate = new AudioFileSourceLittleFS("/deactivate.wav");
-      if (file_deactivate->isOpen()) {
-        wav->begin(file_deactivate, out);
-        triggered = false;
-      } else {
-        Serial.println("Failed to open deactivate.wav");
-      }
-  }
-  
-  if (warning_lights)
-  {
-    loop_warning_lights();
-  }
-  
-
-  previousState = currentState;
-  if (wav->isRunning()) {
-    if (!wav->loop()) {
-      wav->stop();
-    }
-  }
-}
+void loop(){}
 
 void littleFsListDir(const char *dirname) {
   Serial.printf("Listing directory: %s\n", dirname);
@@ -144,23 +188,4 @@ void littleFsListDir(const char *dirname) {
     Serial.printf(" FILE: %s SIZE: %u\n", root.fileName().c_str(), file.size());
     file.close();
   }
-}
-
-void loop_warning_lights(){
-  digitalWrite(WARNING_LEFT_LED, HIGH);
-  delay(500);
-  digitalWrite(WARNING_LEFT_LED, LOW);
-  digitalWrite(WARNING_MIDDLE_LED, HIGH);
-  delay(500);
-  digitalWrite(WARNING_MIDDLE_LED, LOW);
-  digitalWrite(WARNING_RIGHT_LED, HIGH);
-  delay(500);
-  digitalWrite(WARNING_RIGHT_LED, LOW);
-}
-
-void stop_all_lights(){
-  digitalWrite(WARNING_LEFT_LED, HIGH);
-  digitalWrite(WARNING_MIDDLE_LED, LOW);
-  digitalWrite(WARNING_RIGHT_LED, HIGH);
-  digitalWrite(ARMED_LED, LOW);
 }
